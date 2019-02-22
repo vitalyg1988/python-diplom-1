cd6fc437d7333d14946a00190a832763ed3244f2
Vitaly Golubev <vitalyg1988@gmail.com>
2019-02-22 15:47:43 +0400

    correction of mistakes

--- python-diplom-1.py
+++ python-diplom-1.py
@@ -4,17 +4,10 @@ import os
 import requests
 from pprint import pprint
 from time import sleep
-from urllib.parse import urlencode
 
 PATH = os.path.abspath(os.path.dirname(__file__))
 
-
-APP_ID = 6866070
-AUTH_URL = 'https://oauth.vk.com/authorize'
-
-aut_data = dict(client_id=APP_ID, response_type='token', v='5.92')
-
-print('?'.join((AUTH_URL, urlencode(aut_data))))
+token = 'ed1271af9e8883f7a7c2cefbfddfcbc61563029666c487b2f71a5227cce0d1b533c4af4c5b888633c06ae'
 
 
 def get_user_input():
@@ -23,7 +16,8 @@ def get_user_input():
 
 
 class UserVk:
-    TOKEN = 'e7a4b85a9e08dfc28c55cac4886f20fff880450b54324466426155b962825302bc366e3cb9f6c14159bed'
+
+    TOKEN = token
     param = dict(v='5.92', access_token=TOKEN)
 
     @staticmethod
@@ -51,26 +45,70 @@ class UserVk:
         UserVk.param['user_id'] = self.user_id
         method = 'groups.get'
         response = UserVk.get_response(method)
-        return list(map(UserVk, response['response']['items']))
+        try:
+            if 'error' in response:
+                assert response['error']['error_code'] != 6
+                response = response['response'][0]
+        except AssertionError:
+            sleep(0.5)
+            self.get_groups()
+        try:
+            return response['response']['items']
+        except KeyError:
+            return list
 
     def __repr__(self):
         return f'{self.user_id}'
 
-
-# class Groups(UserVk):
-    def description_groups(self, group_list):
-        for group in group_list:
+    # Эта функция пока никак не нужна и не работает как нужно (цикл For), но оставлю как есть
+    def description_group(self, groups_list):
+        descript_list = []
+        for group in groups_list:
             UserVk.param['group_ids'] = group
             UserVk.param['fields'] = 'members_count'
             method = 'groups.getById'
+            response = UserVk.get_response(method)
+            try:
+                if 'error' in response:
+                    assert response['error']['error_code'] != 6
+            except AssertionError:
+                sleep(0.5)
+                self.description_group(groups_list)
             response = UserVk.get_response(method)['response'][0]
-            descript_groups = {'name': response['name'],
-                               'gid': response['id'],
-                               'members_count': response['members_count']}
-            return descript_groups
+            descript_group = {'name': response['name'],
+                              'gid': response['id'],
+                              'members_count': response['members_count']}
+            descript_list.append(descript_group)
+            return descript_list
+
+    # вот здесь вся моя проблема и кроется
+    def get_friends_groups_list(self, friends_list):
+        friends_groups_list = []
+        for friend in friends_list:
+            UserVk.param['user_id'] = friend
+            UserVk.param['extended'] = '1'
+            UserVk.param['fields'] = 'id'
+            method = 'groups.get'
+            response = UserVk.get_response(method)
+            try:
+                if 'error' in response:
+                    assert response['error']['error_code'] != 6
+                    response = response['response'][0]
+            except AssertionError:
+                sleep(0.5)
+                self.get_friends_groups_list(friends_list)
+            try:
+                friends_groups_list.append(response['response']['items'])
+                return friends_groups_list
+            except KeyError:
+                return friends_groups_list
+
+    user_groups = property(get_groups)
+    user_friends = property(get_friends_list)
 
 
 if __name__ == "__main__":
     user = UserVk(get_user_input())
-    print(user.get_groups())
-    print(user.description_groups(user.get_groups()))
+    print(f'список друзей пользователя: {user.user_friends}')
+    print(f'список групп пользователя: {user.user_groups}')
+    pprint(user.get_friends_groups_list(user.get_friends_list))
